3d107f02c561e2cfd6fe42cf6e9d543cd6cabd1e
Eric Allam <eallam@icloud.com>
2026-05-10 17:18:53 +0100

    feat(core): add whisper()

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -16,3 +16,7 @@ export function farewell(name: string): Greeting {
 export function shout(name: string): Greeting {
   return { message: `HEY THERE, ${name.toUpperCase()}!!!`, source: "core" };
 }
+
+export function whisper(name: string): Greeting {
+  return { message: `(psst, hey ${name.toLowerCase()})`, source: "core" };
+}
